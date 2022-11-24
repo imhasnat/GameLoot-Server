5dd5cc33f70cd97602e2b8e3353c5e1df2dc1445
Hasnat Mahbub <imhasnat00@gmail.com>
2022-11-24 16:24:22 +0600

    jwt token for user

--- index.js
+++ index.js
@@ -23,7 +23,16 @@ async function run() {
     try {
         const usersCollection = client.db('resaleProduct').collection('users');
 
-
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query);
+            if (user) {
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+                return res.send({ accessToken: token })
+            }
+            res.status(403).send({ accessToken: '' });
+        })
 
 
 
