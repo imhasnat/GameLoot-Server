9089f8588a74fe767af1dcdf1d8826c7d444f4c2
Hasnat Mahbub <imhasnat00@gmail.com>
2022-11-25 10:39:45 +0600

    verifyJWT token added

--- index.js
+++ index.js
@@ -14,6 +14,21 @@ app.get('/', (req, res) => {
     res.send('Server running');
 })
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send('Unauthorized access');
+    }
+    const token = authHeader.split(' ')[1];
+
+    jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ovbuiyj.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri);
@@ -25,12 +40,14 @@ async function run() {
         const productsCollection = client.db('resaleProduct').collection('products');
         const categoryCollection = client.db('resaleProduct').collection('categories');
 
+
+
         app.get('/jwt', async (req, res) => {
             const email = req.query.email;
             const query = { email: email };
             const user = await usersCollection.findOne(query);
             if (user) {
-                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '24h' });
+                const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '72h' });
                 return res.send({ accessToken: token })
             }
             res.status(403).send({ accessToken: '' });
